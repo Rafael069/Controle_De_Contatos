d87b220e3b5c5d273e45438d5e48d9c6744f560b
Rafael069 <santiagorafael069@gmail.com>
2025-03-04 20:16:47 -0300

    Primeiro commit do sistema MVC - Controle de Contatos

---
+++ ControleDeContatos/Program.cs
@@ -0,0 +1,43 @@
+using ControleDeContatos.Data;
+using ControleDeContatos.Repositorio;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            //builder.Services.AddDbContext<BancoContext>(o => o.UseSqlServer());
+            // Configura a string de conexão a partir do arquivo appsettings.json
+            var connectionString = builder.Configuration.GetConnectionString("DataBase");
+            builder.Services.AddDbContext<BancoContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Controle_De_Contatos MVC
+Gerenciamentos de Contatos utilizando ASPNET MVC, BOOTSTRAP, RAZOR, ENTITY FRAMEWORK E SQL SERVER.
